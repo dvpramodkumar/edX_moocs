5789617633b0d1b8400d85024fde3617c8056810
Pramod Duvvuri <dvpramodkumar@gmail.com>
2019-09-12 22:29:38 -0500

    Added Lecture 1

---
+++ 6.00.1x/Week 1/Lecture_1/sample.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Pramod Duvvuri
+"""
+
+print('Hello World!')
+
+# Types in Python
+
+print(type(3.14))
+print(type(None))
+
+# Arithemtic Operations in Python
+
+print(6 + 12 - 3)
+print(2 * 3.0)
+print(- - 4)
+print(10/3)
+print(10.0/3.0)
+
+a = 3
+print(a + 2.0)
+
+a = a + 1.0
+a
+
+print('Rounded Integer ', round(2.6))
+print(int(2.6))
+
+# str1 = 'hello'
+# str2 = ','
+# str3 = 'world'
