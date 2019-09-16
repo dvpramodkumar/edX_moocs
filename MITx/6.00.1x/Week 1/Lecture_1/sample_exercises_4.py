8524afa7241e3ddaa20da69285941a8f10643180
Pramod Duvvuri <dvpramodkumar@gmail.com>
2019-09-16 15:39:16 -0500

    Update 9/16

--- MITx/6.00.1x/Week 1/Lecture_1/sample_exercises_4.py
+++ MITx/6.00.1x/Week 1/Lecture_1/sample_exercises_4.py
@@ -7,3 +7,13 @@ while iteration < 5:
         count += 1
     print("Iteration " + str(iteration) + "; count is: " + str(count))
     iteration += 1
+
+
+# Test-1
+for iteration in range(5):
+    count = 0
+    while True:
+        for letter in "hello, world":
+            count += 1
+        print("Iteration " + str(iteration) + "; count is: " + str(count))
+        break
