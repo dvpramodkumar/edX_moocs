ab3c768de6986e09a4ba1f3f602ee328f7664373
Pramod Duvvuri <dvpramodkumar@gmail.com>
2019-09-16 19:42:58 -0500

    Added week 2/Lecture 2 Programs

--- MITx/6.00.1x/Week 2/Lecture_2/strings.py
+++ MITx/6.00.1x/Week 2/Lecture_2/strings.py
@@ -1,4 +1,19 @@
+# String and Loops
+
 s1 = "abcdefgh"
 
 print('String is: ', s1)
 print('String reversed is: ', s1[::-1])
+
+
+# Code Sample
+
+iteration = 0
+count = 0
+while iteration < 5:
+    # the variable 'letter' in the loop stands for every
+    # character, including spaces and commas!
+    for letter in "hello, world":
+        count += 1
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1
