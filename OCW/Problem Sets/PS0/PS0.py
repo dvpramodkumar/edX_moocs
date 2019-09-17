0abe64978149f2555d01bc02d7d6b019847a5f76
Pramod Duvvuri <dvpramodkumar@gmail.com>
2019-09-16 19:00:44 -0500

    Added OCW PS0

---
+++ OCW/Problem Sets/PS0/PS0.py
@@ -0,0 +1,23 @@
+import math
+
+#x = int(input('Enter the number \'x\': '))
+#y = int(input('Enter another number: '))
+
+x = 2
+y = 5
+
+print('x = ', x)
+print('y = ', y)
+print(x, 'to the power of',y,'is', int(math.pow(x,y)))
+print('log(x) = ', math.log2(x))
+
+
+# -- 2008 PS0
+
+#last_name = input('Enter you Last Name:')
+#first_name = input('Enter your First Name:')
+
+last_name = 'Doe'
+first_name = 'John'
+
+print(first_name + ', ' + last_name)
